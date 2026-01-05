57c0501db1690a5fda3a20ab05df150f5fddb6ad
nikit <nikit@LAPTOP-0H0AJTB3>
2026-01-05 22:57:04 +0300

    add logic if else

--- ConsoleApp4/GradeCheck.cs
+++ ConsoleApp4/GradeCheck.cs
@@ -5,6 +5,28 @@
         Console.WriteLine("Введите оценку от 0 до 100: ");
         string input = Console.ReadLine();
         bool isNumber = int.TryParse(input, out int score);
+        if (!isNumber)
+        {
+            Console.WriteLine("Вы ввели не цифру!");
+        }
+        if (score < 0 || score > 100) 
+            Console.WriteLine("Неккоретная оценка!");
+        else if (score > 90)
+        {
+            Console.WriteLine("Otlichno")
+        }
+        else if (score > 75)
+        {
+            Console.WriteLine("horosho")
+        }
+        else if (score > 60)
+        {
+            Console.WriteLine("Ydovotvoritlno");
+        }
+        else
+        {
+            Console.WriteLine("HUYNA");
+        }
 
     }
 }
