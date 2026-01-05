0022726b2eb1f1200cc38930719b021bf9c6671b
nikit <nikit@LAPTOP-0H0AJTB3>
2026-01-05 19:10:55 +0300

    add logic age

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,3 +1,24 @@
 ﻿Console.WriteLine("Введите возраст: ");
 string input = Console.ReadLine();
 bool isNumber = int.TryParse(input, out int age);
+if (!int.TryParse(input, out age))
+{
+    Console.WriteLine("Вы ввели не число!");
+}
+else if (age < 0)
+{
+    Console.WriteLine("Возраст не может быть отрицательным!");
+}
+else if (age < 18)
+{
+    Console.WriteLine("Не совершенно летний пользователь!");
+}
+else if (age <= 64)
+{
+    Console.WriteLine("Доступ разрешен!");
+}
+else if (age > 64)
+{
+    Console.WriteLine("Пенсионный возраст!");
+}
+    
