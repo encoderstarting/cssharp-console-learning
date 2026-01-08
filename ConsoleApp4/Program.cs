54b12b073938adf376abe95ea62dacb818792696
encoderstarting <112802133+encoderstarting@users.noreply.github.com>
2026-01-09 02:30:25 +0300

    Добавление ошибки

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -3,16 +3,21 @@ Console.WriteLine("Выберите что хотите запустить: ");
 Console.WriteLine("1 - Проверка баллов");
 Console.WriteLine("2 - Проверка возраста");
 Console.WriteLine("3 - Калькулятор");
-string num  = Console.ReadLine();
-if (num == "1")
+bool number = int.TryParse(Console.ReadLine(), out int num);
+if (!number)
+{
+    Console.WriteLine("Ошибка! Повторите попытку!");
+    return;
+}
+else if (num == 1)
 {
     GradeCheck.Run();
 }
-else if (num  == "2")
+else if (num  == 2)
 {
     CheckAge.Run();
 }
-else if (num == "3")
+else if (num == 3)
 {
     calculator.Run();
 }
